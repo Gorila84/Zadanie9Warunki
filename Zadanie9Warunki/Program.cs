6b96b1352701b525ccaabd5ebfe8285c3e30815b
Maciej Sikora <maciek.sikora84@gmail.com>
2024-01-24 21:17:18 +0100

    Add project files.

---
+++ Zadanie9Warunki/Program.cs
@@ -0,0 +1,30 @@
+﻿double temp;
+
+Console.WriteLine("Wpisz temperaturę");
+var enteredTemp = Console.ReadLine();
+double.TryParse(enteredTemp, out temp);
+
+if(temp < 0)
+{
+    Console.WriteLine("Cholernie piździ");
+}
+else if(temp >= 0 && temp < 10)
+{
+    Console.WriteLine("zimno");
+}
+else if (temp >= 10 && temp < 20)
+{
+    Console.WriteLine("chłodno");
+}
+else if (temp >= 20 && temp < 30)
+{
+    Console.WriteLine("w sam raz");
+}
+else if (temp >= 30 && temp < 40)
+{
+    Console.WriteLine("zaczyna być słabo, bo gorąco");
+}
+else if (temp >= 40)
+{
+    Console.WriteLine("a weź wyprowadzam się na Alaskę.");
+}
